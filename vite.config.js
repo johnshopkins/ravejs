7f0459c102e043784a4420dbc7b49f4c0e8de7fd
Jen Wachter <jwachter@jhu.edu>
2026-01-14 14:23:28 -0500

    Compiled library

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,17 @@ import eslint from 'vite-plugin-eslint2';
 import stylelint from 'vite-plugin-stylelint';
 
 export default defineConfig({
-  // build: {
-  //   target: 'es2015',
-  //   // emptyOutDir: false,
-  //   lib: {
-  //     entry: 'src/js/main.js',
-  //     name: 'ravejs',
-  //     fileName: format => `main.${format}.js`
-  //   },
-  // },
+  build: {
+    target: 'es2015',
+    lib: {
+      entry: 'src/main.js',
+      name: 'ravejs',
+      formats: ['iife'],
+      // fileName: format => `main.${format}.js`,
+      fileName: format => 'main.js',
+      cssFileName: 'main',
+    },
+  },
   plugins: [
     eslint(),
     stylelint(),
